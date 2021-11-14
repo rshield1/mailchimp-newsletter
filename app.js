0648ca6f83c25db207096d4cc3e56218501da221
Robert Shields <you@example.com>
2021-11-13 21:25:34 -0800

    added static for public pages

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const request = require("request");
 
 
 const app = express();
+//use static pages from local server
+app.use(express.static("public"))
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/signup.html")
+})
 
 app.listen(3000, () =>{
     console.log("Server is running on port 3000")
