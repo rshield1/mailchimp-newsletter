a2fa614d911495cd5a52ff5a6501a61c398d87b4
Robert Shields <you@example.com>
2021-11-13 21:35:57 -0800

    able to send data to server backend for parsing

--- app.js
+++ app.js
@@ -6,10 +6,23 @@ const request = require("request");
 const app = express();
 //use static pages from local server
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/signup.html")
 })
+
+app.post("/", (req, res) =>{
+
+    let firstName = req.body.fname
+    let lastName = req.body.lname
+    let email = req.body.email
+
+    console.log(firstName, lastName, email);
+})
+
+
 
 app.listen(3000, () =>{
     console.log("Server is running on port 3000")
